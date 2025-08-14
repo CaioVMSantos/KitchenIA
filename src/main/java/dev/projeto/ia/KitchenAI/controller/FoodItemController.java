c2482fc29060ab1659801e28f98605157f0108dd
c4ioba <caiovictorcvm10@gmail.com>
2025-08-14 16:06:56 -0300

    feat: Implementa, db.migration e métodos na Controller e na Service.

--- src/main/java/dev/projeto/ia/KitchenAI/controller/FoodItemController.java
+++ src/main/java/dev/projeto/ia/KitchenAI/controller/FoodItemController.java
@@ -2,11 +2,9 @@ package dev.projeto.ia.KitchenAI.controller;
 
 import dev.projeto.ia.KitchenAI.model.FoodItem;
 import dev.projeto.ia.KitchenAI.service.FoodItemService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,19 +19,58 @@ public class FoodItemController {
     }
 
     //POST
-    @PostMapping
+    @PostMapping("/criar")
     public ResponseEntity<FoodItem> createFood(@RequestBody FoodItem foodItem) {
             FoodItem salvo = foodItemService.saveItem(foodItem);
             return ResponseEntity.ok(salvo);
     }
 
     //GET
-    public List<FoodItem> getFoodItems() {
+    @GetMapping("/listar")
+    public ResponseEntity<List<FoodItem>> getFoodItems() {
+            List<FoodItem> listar = foodItemService.itemsList();
+            return ResponseEntity.ok(listar);
+    }
 
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<?> getFoodItensId(@RequestParam Long id){
+        FoodItem foodId = foodItemService.findById(id);
+        if(foodId != null){
+            return ResponseEntity.ok(foodId);
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Item com id " +id+ " não encontrado!");
+        }
+    }
+
+    //UPDATE
+    @PutMapping("atualizar/{id}")
+    public ResponseEntity<String> updateItem (@PathVariable Long id, @RequestBody FoodItem foodItem) {
+        if(foodItemService.itemsList() != null){
+            foodItemService.alterItem(id, foodItem);
+            return ResponseEntity.status(HttpStatus.OK).
+                    body("Item alterado com sucesso");
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).
+                    body("Item com id " +id+ " não encontrado!");
+        }
+    }
+
+    //DELETE
+    @DeleteMapping("deletar/{id}")
+    public ResponseEntity<String> deleteItem(@PathVariable Long id){
+        if(foodItemService.itemsList() != null){
+            foodItemService.deleteItem(id);
+            return ResponseEntity.ok("Item deletado com sucesso!");
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Item com id " +id+ " não encontrado!");
+        }
 
     }
-    //UPDATE
-    //DELETE
 
 
 
--- src/main/java/dev/projeto/ia/KitchenAI/service/FoodItemService.java
+++ src/main/java/dev/projeto/ia/KitchenAI/service/FoodItemService.java
@@ -5,6 +5,7 @@ import dev.projeto.ia.KitchenAI.repository.FoodItemRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FoodItemService {
@@ -21,8 +22,32 @@ public class FoodItemService {
     }
 
     //listar itens
-    public List<FoodItem> ItemsList() {
+    public List<FoodItem> itemsList() {
         return repository.findAll();
+    }
+
+    //alterar item
+    public FoodItem alterItem (Long id, FoodItem foodItem) {
+        Optional<FoodItem> optionalFoodItem = repository.findById(id); //retorna o optional com objeto ou nulo.
+        if (optionalFoodItem.isPresent()) {
+            foodItem.setId(id);
+            return repository.save(foodItem);
+        }
+        return null;
+    }
+
+    //listar por id
+    public FoodItem findById(Long id) {
+        Optional<FoodItem> optionalFoodItem = repository.findById(id);
+        if (optionalFoodItem.isPresent()) {
+            return optionalFoodItem.get();
+        }
+        return null;
+    }
+
+    //deletar item
+    public void deleteItem(Long id){
+        repository.deleteById(id);
     }
 
 }
