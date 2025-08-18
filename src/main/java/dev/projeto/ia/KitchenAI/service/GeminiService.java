b8ac286ebbd74ad785db414cd361deac22247079
c4ioba <caiovictorcvm10@gmail.com>
2025-08-18 19:37:34 -0300

    style: Comentários

--- src/main/java/dev/projeto/ia/KitchenAI/service/GeminiService.java
+++ src/main/java/dev/projeto/ia/KitchenAI/service/GeminiService.java
@@ -27,12 +27,12 @@ public class GeminiService {
     }
 
     public Mono<String> generateRecipe(String prompt){
-        return webClient.post()
+        return webClient.post()//basicamente setando as informações que são requeridas para usar a api do gemini.
                 .uri("/models/gemini-2.0-flash:generateContent")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .header("X-goog-api-key", geminiApiKey)
-                .bodyValue(buildRequest(prompt))
-                .retrieve()
+                .header("X-goog-api-key", geminiApiKey) //geminiApiKey - está sendo usada como variável de ambiente por segurança.
+                .bodyValue(buildRequest(prompt)) //preciso entregar um body com o prompt, por isso crio outro metodo pra fazer isso.
+                .retrieve() //executa a requisição Http.
                 .bodyToMono(String.class);
     }
 
