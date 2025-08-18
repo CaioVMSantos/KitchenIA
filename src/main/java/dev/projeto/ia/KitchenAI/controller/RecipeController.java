9ddce042206a46007c0fcb1a4d87aa30bcd0e245
c4ioba <caiovictorcvm10@gmail.com>
2025-08-18 20:18:01 -0300

    feat: Transforma o 'generateRecipe' em GetMapping, sendo assim, não é necessário corpo de requisição e agora pode-se tirar os alimentos do banco de dados.

--- src/main/java/dev/projeto/ia/KitchenAI/controller/RecipeController.java
+++ src/main/java/dev/projeto/ia/KitchenAI/controller/RecipeController.java
@@ -1,9 +1,13 @@
 package dev.projeto.ia.KitchenAI.controller;
 
+import dev.projeto.ia.KitchenAI.model.FoodItem;
+import dev.projeto.ia.KitchenAI.service.FoodItemService;
 import dev.projeto.ia.KitchenAI.service.GeminiService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -11,15 +15,19 @@ import java.util.Map;
 public class RecipeController {
 
     private GeminiService geminiService;
+    private FoodItemService foodItemService;
 
-    public RecipeController(GeminiService geminiService) {
+    public RecipeController(GeminiService geminiService, FoodItemService foodItemService) {
         this.geminiService = geminiService;
+        this.foodItemService = foodItemService;
     }
 
-    @PostMapping("/generateRecipe")
-    public Mono<String> generateRecipe(@RequestBody Map<String, String> request){ //Mono: Assíncrono, faz uma especie de promessa que diz que ao enviar algo, algo irá retornar, mesmo que não seja no mesmo periodo de tempo.
-        String prompt = request.get("prompt");
-        return geminiService.generateRecipe(prompt);
+    @GetMapping("/generateRecipe")
+    public Mono<ResponseEntity<String>> generateRecipe(){ //Mono: Assíncrono, faz uma especie de promessa que diz que ao enviar algo, algo irá retornar, mesmo que não seja no mesmo periodo de tempo.
+        List<FoodItem> foodItems = foodItemService.itemsList();
+        return geminiService.generateRecipe(foodItems)
+                .map(recipe -> ResponseEntity.ok().body(recipe))
+                .defaultIfEmpty(ResponseEntity.noContent().build());
     }
 
 }
--- src/main/java/dev/projeto/ia/KitchenAI/service/GeminiService.java
+++ src/main/java/dev/projeto/ia/KitchenAI/service/GeminiService.java
@@ -1,5 +1,6 @@
 package dev.projeto.ia.KitchenAI.service;
 
+import dev.projeto.ia.KitchenAI.model.FoodItem;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpHeaders;
@@ -11,6 +12,7 @@ import reactor.core.publisher.Mono;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class GeminiService {
@@ -26,18 +28,24 @@ public class GeminiService {
                 .build();
     }
 
-    public Mono<String> generateRecipe(String prompt){
+    public Mono<String> generateRecipe(List<FoodItem> foodItems) {
+
+        String ingredients = foodItems.stream()
+                .map(item -> String.format("%s (%s) - Quantidade: %d, Validade: %s",
+                        item.getName(),item.getCategory(), item.getQuantity(), item.getValidity()))
+                .collect(Collectors.joining("\n"));
+
+        String prompt = "A partir de agora, você é um cozinheiro, baseado no meu banco de dados, faça uma receita com os seguintes itens: " + ingredients;
+
+        Map<String, Object> buildRequest = Map.of("contents", List.of(Map.of("parts", List.of(Map.of("text", prompt)))));
+
         return webClient.post()//basicamente setando as informações que são requeridas para usar a api do gemini.
                 .uri("/models/gemini-2.0-flash:generateContent")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .header("X-goog-api-key", geminiApiKey) //geminiApiKey - está sendo usada como variável de ambiente por segurança.
-                .bodyValue(buildRequest(prompt)) //preciso entregar um body com o prompt, por isso crio outro metodo pra fazer isso.
+                .bodyValue(buildRequest) //preciso entregar um body com o prompt, por isso crio outro metodo pra fazer isso.
                 .retrieve() //executa a requisição Http.
                 .bodyToMono(String.class);
-    }
-
-    private Map<String, Object> buildRequest(String prompt){
-        return Map.of("contents", List.of(Map.of("parts", List.of(Map.of("text", prompt)))));
     }
 
 
