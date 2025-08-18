648c1bcdb33a0facffcaed0d8a31d1f39eb37b8d
c4ioba <caiovictorcvm10@gmail.com>
2025-08-18 16:51:08 -0300

    feat: Adiciona Gemini Service com chave da api e a Controller, possibilitanso requisições com segurança.

--- src/main/java/dev/projeto/ia/KitchenAI/controller/RecipeController.java
+++ src/main/java/dev/projeto/ia/KitchenAI/controller/RecipeController.java
@@ -1,8 +1,26 @@
 package dev.projeto.ia.KitchenAI.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import dev.projeto.ia.KitchenAI.service.GeminiService;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @RestController
+@RequestMapping("/gemini")
 public class RecipeController {
+
+    private GeminiService geminiService;
+
+    public RecipeController(GeminiService geminiService) {
+        this.geminiService = geminiService;
+    }
+
+    @PostMapping("/generateRecipe")
+    public Mono<String> generateRecipe(@RequestBody Map<String, String> request){ //Mono: Assíncrono, faz uma especie de promessa que diz que ao enviar algo, algo irá retornar, mesmo que não seja no mesmo periodo de tempo.
+        String prompt = request.get("prompt");
+        return geminiService.generateRecipe(prompt);
+    }
+
 }
 
---
+++ src/main/java/dev/projeto/ia/KitchenAI/service/GeminiService.java
@@ -0,0 +1,44 @@
+package dev.projeto.ia.KitchenAI.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class GeminiService {
+
+    private final WebClient webClient;
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+
+    public GeminiService(WebClient webClient) {
+        this.webClient = WebClient.builder()
+                .baseUrl("https://generativelanguage.googleapis.com/v1beta")
+                .build();
+    }
+
+    public Mono<String> generateRecipe(String prompt){
+        return webClient.post()
+                .uri("/models/gemini-2.0-flash:generateContent")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header("X-goog-api-key", geminiApiKey)
+                .bodyValue(buildRequest(prompt))
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    private Map<String, Object> buildRequest(String prompt){
+        return Map.of("contents", List.of(Map.of("parts", List.of(Map.of("text", prompt)))));
+    }
+
+
+}
