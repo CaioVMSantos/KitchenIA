f9cfde5460c8db1ef8d19b12e687a551ff12fe69
jhean.almeida <jhean.almeida@defesa.gov.br>
2025-08-19 16:52:03 -0300

    feat: Documentação da API com Swagger

--- src/main/java/dev/projeto/ia/KitchenAI/config/SwaggerConfig.java
+++ src/main/java/dev/projeto/ia/KitchenAI/config/SwaggerConfig.java
@@ -1,4 +1,23 @@
 package dev.projeto.ia.KitchenAI.config;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI kitchenOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(" KITCHENIA API")
+                        .description("Documentação da API do sistema Kitchen IA")
+                        .version("1.0.0"));
+    }
+
+
+
 }
