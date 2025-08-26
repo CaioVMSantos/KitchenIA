8dd9947adc9065ca2b76b387c377bc3f7e4385f9
CaioVMSantos <caiovictorcvm10@gmail.com>
2025-08-26 11:52:07 -0300

    chore(config): Atualiza migration para o Postgres

--- src/main/resources/db/migration/V1__create_food_item_table.sql
+++ src/main/resources/db/migration/V1__create_food_item_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE food_item (
-        id BIGINT PRIMARY KEY AUTO_INCREMENT,
-        name VARCHAR(255) NOT NULL,
-        category VARCHAR(100),
-        quantity INT,
-        validate DATE
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    category VARCHAR(100),
+    quantity INT,
+    validate DATE
 );
+
